12c34a9fb244c305875c4990683f3f85e8821a7a
Rajat  Singh <129305916+raja12351@users.noreply.github.com>
2023-05-29 10:36:35 +0530

    Update Meeting.java

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -3,17 +3,18 @@ package com.driver;
 import java.time.LocalTime;
 
 public class Meeting {
-    private static LocalTime startTime;
-    private static LocalTime endTime;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
     public Meeting(LocalTime startTime, LocalTime endTime){
         this.startTime = startTime;
         this.endTime = endTime;
     }
-    public static boolean findSlot(Meeting meeting){
-        if(meeting.startTime==endTime){
-            return false;
-        }
-        return true;
+    public LocalTime getStartTime(){
+        return startTime;
     }
+    public LocalTime getEndTime(){
+        return endTime;
+    }
+
 }
